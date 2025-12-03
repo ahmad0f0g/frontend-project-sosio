7e272ffb6ae1b49b7f47efb41167ec528828b1d8
ahmad0f0g <fadilahfakih0@gmail.com>
2025-12-03 13:41:29 +0700

    edit backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,10 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: "https://temusgd.netlify.app",
+  methods: ["GET", "POST"]
+}));
 app.use(express.json());
 
 // 🔹 CONNECT TO MONGODB
@@ -24,5 +26,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`)); 
